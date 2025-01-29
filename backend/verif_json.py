cb3db8b067ccc7f4cceb389682b6f321058dbc09
jass228 <assouma@et.esiea.fr>
2025-01-29 21:15:54 +0100

    airflow - extract data from tmdb api

---
+++ backend/verif_json.py
@@ -0,0 +1,35 @@
+import os
+import json
+
+def check_json_structure(file_path, required_keys):
+    """Check if the JSON file has the required keys"""
+    
+    if not os.path.exists(file_path):
+        print(f"❌ The file {file_path} does not exist")
+        return False
+    
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        
+        if not isinstance(data, list):
+            print(f"❌ The file {file_path} is not a JSON object")
+            return False
+        
+        for item in data[:5]:
+            missing_keys = [key for key in required_keys if key not in item]
+            if missing_keys:
+                print(f"❌ The file {file_path} does not have the keys: {missing_keys}")
+                return False
+        
+        print(f"✅ The file {file_path} has the required keys")
+        return True
+    except Exception as e:
+        print(f"❌ Error while reading the file {file_path}: {str(e)}")
+        return False
+
+required_keys_movies = ['id', 'title', 'release_date', 'vote_average', 'genre_ids']
+required_keys_series = ['id', 'name', 'first_air_date', 'vote_average', 'genre_ids']
+
+check_json_structure('./tmdb_data/netflix_movies.json', required_keys_movies)
+check_json_structure('./tmdb_data/netflix_series.json', required_keys_series)
---
+++ todo.md
@@ -0,0 +1,9 @@
+## Plan d'Action
+
+- [x] Mise place de l'environnement (Airflow, PostgreSQL, MongoDB, FastAPI, NextJS)
+- [x] Création du pipeline ETL : Extraction des données depuis l'API TMDB
+- [ ] Stockage des données: Chargement dans PostgreSQL et MongoDB
+- [ ] Création de l'API FastAPI pour exposer les données
+- [ ] Développement de l'interface web avec NextJS
+- [ ] Ajout des visualisations interactives
+- [ ] Finalisation du projet
