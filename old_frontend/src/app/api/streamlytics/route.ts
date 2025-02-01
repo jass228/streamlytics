d9b70de88347f141c889f835832bde9c99243064
jass228 <assouma@et.esiea.fr>
2025-02-01 01:43:46 +0100

    last-work-31-01-25

--- old_frontend/src/app/api/streamlytics/route.ts
+++
@@ -1,8 +0,0 @@
-import { StreamlyticsContent } from "@/type/api";
-import { NextResponse } from "next/server";
-
-const stream: StreamlyticsContent[] = [];
-
-export async function GET() {
-  return NextResponse.json(stream);
-}
